5bfb0574c3fd50d264ae65e6677509b3de15dac1
rajmohanperiyasamy <rajmohanjr@gmail.com>
2015-07-15 01:18:44 +0530

    Update settings.py

--- snap/settings.py
+++ snap/settings.py
@@ -35,7 +35,7 @@ EMAIL_USE_TLS = True
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'rajmohanjr@gmail.com'
-EMAIL_HOST_PASSWORD = 'doublespring1'
+EMAIL_HOST_PASSWORD = 'password'
 DEFAULT_FROM_EMAIL = ''
 SERVER_EMAIL = '<address>'
 
